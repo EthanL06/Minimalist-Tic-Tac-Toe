6720fe6abbd38cbbfcfbb84f95f35ba49d8d6f6a
EthanL06 <ethanlanting@gmail.com>
2021-08-05 11:40:13 -0500

    Made code more readable and added a click cell animation

--- game.js
+++ game.js
@@ -12,7 +12,7 @@ let board = [
   []
 ];
 
-// Event listener for start button click
+// START BUTTON EVENT LISTENER
 $(".start-btn").click(function() {
 
   $(this).off("click");
@@ -42,6 +42,11 @@ $(".start-btn").click(function() {
 });
 
 
+/*
+
+GAME FUNCTIONS
+
+*/
 
 function startGame() {
   board = [[],[],[]];
@@ -50,58 +55,42 @@ function startGame() {
   cellClickListener();
 }
 
-function restartEvent() {
-  $(document).on('click', function() {
-    $(this).off('click');
+function updateBoard(classes) {
+  let cell = classes.substr(0, classes.indexOf(" "));
+  let player = playerOneTurn ? players.ONE : players.TWO;
 
-    restartAnimation();
-  })
-}
-
-function restartAnimation() {
-  setTimeout(function() {
-    openingTheme();
-
-    setTimeout(function() {
-
-      $(".turn-indicator").transit({opacity: 0}, 500);
-      $(".game-subtitle").transit({opacity: 0}, 800, function() {
-        $(this).css("visibility", "hidden");
-      })
-      $(".board").transit({opacity: 0}, 1000, function() {
-        resetCells();
-        $(".game-subtitle").css("display", "none");
-      });
-
-      setTimeout(function() {
-        $(".turn-indicator").css("scale", "0.3").transit({opacity: 1, scale: 1});
-        $(".board").css("scale", "0.3").transit({opacity: 1, scale: 1});
-        $(".game-subtitle").css("visibility", "visible");
-        playWhoosh();
-        startGame();
-      }, 2500);
-    }, 100);
-  }, 500);
-}
-
-
-function resetCells() {
-  $("td").html("");
-}
-function cellClickListener() {
-  $("td").click(function() {
-
-    updateBoard($(this).attr("class"));
-    setCellImage($(this));
-    placeAudio();
-
-    playerOneTurn = !playerOneTurn;
-    setGameHeading();
-
-    $(this).off('click');
-
-    checkGameStatus();
-  });
+  switch(cell) {
+    case "one":
+      board[0][0] = player;
+      break;
+    case "two":
+      board[0][1] = player;
+      break;
+    case "three":
+      board[0][2] = player;
+      break;
+    case "four":
+      board[1][0] = player;
+      break;
+    case "five":
+      board[1][1] = player;
+      break;
+    case "six":
+      board[1][2] = player;
+      break;
+    case "seven":
+      board[2][0] = player;
+      break;
+    case "eight":
+      board[2][1] = player;
+      break;
+    case "nine":
+      board[2][2] = player;
+      break;
+    default:
+      console.error("Error at switch statement. Cell = " + cell);
+      break;
+  }
 }
 
 function checkGameStatus() {
@@ -116,28 +105,6 @@ function checkGameStatus() {
     onTie();
     setTimeout(restartEvent, 1100);
   }
-}
-
-function onWin(winner) {
-  gameHeading.text(`Player ${winner} has won.`);
-  disableCellEventListeners();
-  setTimeout(function() {
-    playWinAudio();
-    gameSubtitle(true)
-  }, 100);
-}
-
-function onTie() {
-  gameHeading.text("The game was a tie.");
-  disableCellEventListeners();
-  setTimeout(function() {
-    playTieAudio();
-    gameSubtitle(true);
-  }, 100);
-}
-
-function disableCellEventListeners() {
-  $("td").off('click');
 }
 
 function isBoardFull() {
@@ -210,43 +177,128 @@ function checkWin() {
   return false;
 }
 
-function updateBoard(classes) {
-  let cell = classes.substr(0, classes.indexOf(" "));
-  let player = playerOneTurn ? players.ONE : players.TWO;
 
-  switch(cell) {
-    case "one":
-      board[0][0] = player;
-      break;
-    case "two":
-      board[0][1] = player;
-      break;
-    case "three":
-      board[0][2] = player;
-      break;
-    case "four":
-      board[1][0] = player;
-      break;
-    case "five":
-      board[1][1] = player;
-      break;
-    case "six":
-      board[1][2] = player;
-      break;
-    case "seven":
-      board[2][0] = player;
-      break;
-    case "eight":
-      board[2][1] = player;
-      break;
-    case "nine":
-      board[2][2] = player;
-      break;
-    default:
-      console.error("Error at switch statement. Cell = " + cell);
-      break;
-  }
+
+
+
+/*
+
+EVENT LISTENER FUNCTIONS
+
+*/
+
+
+// When a cell is clicked
+function cellClickListener() {
+  $("td").click(function() {
+
+    updateBoard($(this).attr("class"));
+    setCellImage($(this));
+    placeAudio();
+
+    playerOneTurn = !playerOneTurn;
+    setGameHeading();
+
+    $(this).off('click');
+
+    checkGameStatus();
+  });
 }
+
+function disableCellEventListeners() {
+  $("td").off('click');
+}
+
+
+
+/*
+
+ON EVENT FUNCTIONS
+
+*/
+
+function onWin(winner) {
+  gameHeading.text(`Player ${winner} has won.`);
+  disableCellEventListeners();
+  setTimeout(function() {
+    playWinAudio();
+    gameSubtitle(true)
+  }, 100);
+}
+
+function onTie() {
+  gameHeading.text("The game was a tie.");
+  disableCellEventListeners();
+  setTimeout(function() {
+    playTieAudio();
+    gameSubtitle(true);
+  }, 100);
+}
+
+
+
+
+
+
+/*
+
+RESTART FUNCTIONS
+
+*/
+
+function restartEvent() {
+  $(document).on('click', function() {
+    $(this).off('click');
+
+    restartAnimation();
+  })
+}
+
+function restartAnimation() {
+  setTimeout(function() {
+    openingTheme();
+
+    setTimeout(function() {
+
+      $(".turn-indicator").transit({opacity: 0}, 500);
+      $(".game-subtitle").transit({opacity: 0}, 800, function() {
+        $(this).css("visibility", "hidden");
+      })
+      $(".board").transit({opacity: 0}, 1000, function() {
+        resetCells();
+        gameSubtitle(false);
+      });
+
+      setTimeout(function() {
+        $(".turn-indicator").css("scale", "0.3").transit({opacity: 1, scale: 1});
+        $(".board").css("scale", "0.3").transit({opacity: 1, scale: 1});
+        $(".game-subtitle").css("visibility", "visible");
+        playWhoosh();
+        startGame();
+      }, 2500);
+    }, 100);
+  }, 500);
+}
+
+function resetCells() {
+  $("td").html("");
+}
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+SET FUNCTIONS
+
+*/
 
 function setGameHeading() {
   if (playerOneTurn) {
@@ -262,15 +314,61 @@ function setCellImage(cell) {
   } else {
     cell.html(`<img src='images/o.png' draggable='false'>`)
   }
+
+  cellImgAnimation(cell);
 }
 
-function gameSubtitle(turnOn) {
+
+
+
+
+/*
+
+ANIMATION FUNCTIONS
+
+*/
+
+function fadeOut(selector, duration) {
+  $(`${selector}`).animate({
+    opacity: 0
+  }, {
+    duration: duration,
+    queue: false
+  });
+}
+
+function gameSubtitle(turnOn) { // refactor
   if (turnOn) {
     $(".game-subtitle").css("display", "block");
     $(".game-subtitle").transition({opacity: 1.0, y: '30px'});
   } else {
     $(".game-subtitle").css("display", "none");
   }
+}
+
+function cellImgAnimation(cell) {
+  let image = cell.children("img");
+  image.css("scale", "0.5");
+  image.transition({scale: 1.0}, 150);
+
+}
+
+
+
+/*
+
+SFX/AUDIO FUNCTIONS
+
+*/
+
+function openingTheme() {
+  let audio = new Audio(`sounds/game_start.wav`);
+  audio.play();
+}
+
+function playWhoosh() {
+  let audio = new Audio('sounds/whoosh.mp3');
+  audio.play();
 }
 
 function placeAudio() {
@@ -285,16 +383,6 @@ function placeAudio() {
   audio.play();
 }
 
-// Fade out animation
-function fadeOut(selector, duration) {
-  $(`${selector}`).animate({
-    opacity: 0
-  }, {
-    duration: duration,
-    queue: false
-  });
-}
-
 function playTieAudio() {
   let audio = new Audio('sounds/error.wav');
   audio.play();
@@ -302,15 +390,5 @@ function playTieAudio() {
 
 function playWinAudio() {
   let audio = new Audio('sounds/win.wav');
-  audio.play();
-}
-
-function playWhoosh() {
-  let audio = new Audio('sounds/whoosh.mp3');
-  audio.play();
-}
-// Plays opening theme
-function openingTheme() {
-  let audio = new Audio(`sounds/game_start.wav`);
   audio.play();
 }
